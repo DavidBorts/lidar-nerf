719652df458f21554cf6097c5ae5f1dda2cd30b0
DavidBorts <Davidaborts@gmail.com>
2024-01-11 00:18:13 -0500

    deleted extraneous code and imports

--- preprocess/eth_loader.py
+++ preprocess/eth_loader.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 import numpy as np
-import camtools as ct
 import json
 import tqdm
 
@@ -155,44 +154,8 @@ class ETHLoader:
             # use gnss2world to compute lidar2world for this frame
             lidar2world = np.matmul(gnss2world, lidar2gnss)
             lidar2world_dict[lidar_frame] = lidar2world
-
-        raise NotImplementedError()
-        data_poses_dir = self.data_poses_dir / f"{sequence_name}_sync"
-        assert data_poses_dir.is_dir()
-
-        # IMU to world transformation (poses.txt).
-        poses_path = data_poses_dir / "poses.txt"
-        imu_to_world_dict = dict()
-        frame_ids = []
-        for line in np.loadtxt(poses_path):
-            frame_id = int(line[0])
-            frame_ids.append(frame_id)
-            imu_to_world = line[1:].reshape((3, 4))
-            imu_to_world_dict[frame_id] = imu_to_world
-
-        # Camera to IMU transformation (calib_cam_to_pose.txt).
-        cam_to_imu_path = self.calibration_dir / "calib_cam_to_pose.txt"
-        with open(cam_to_imu_path, "r") as fid:
-            cam_00_to_imu = KITTI360Loader._read_variable(fid, "image_00", 3, 4)
-            cam_00_to_imu = ct.convert.pad_0001(cam_00_to_imu)
-
-        # Camera00 to Velo transformation (calib_cam_to_velo.txt).
-        cam00_to_velo_path = self.calibration_dir / "calib_cam_to_velo.txt"
-        with open(cam00_to_velo_path, "r") as fid:
-            line = fid.readline().split()
-            line = [float(x) for x in line]
-            cam_00_to_velo = np.array(line).reshape(3, 4)
-            cam_00_to_velo = ct.convert.pad_0001(cam_00_to_velo)
-
-        # Compute velo_to_world
-        velo_to_world_dict = dict()
-        for frame_id in frame_ids:
-            imu_to_world = imu_to_world_dict[frame_id]
-            cam_00_to_world_unrec = imu_to_world @ cam_00_to_imu
-            velo_to_world = cam_00_to_world_unrec @ np.linalg.inv(cam_00_to_velo)
-            velo_to_world_dict[frame_id] = ct.convert.pad_0001(velo_to_world)
-
-        return velo_to_world_dict
+        
+        return lidar2world_dict
 
     def load_lidars(self, sequence_name, train_frame_ids, test_frame_ids):
         """
